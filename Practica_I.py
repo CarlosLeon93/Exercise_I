830a3d035adea844651bce92301fc87d8949322f
cals_1993@hotmail.com <cals_1993@hotmail.com>
2019-11-17 22:05:05 +0100

    Version 1.0

--- Practica_I.py
+++ Practica_I.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 import sqlite3
 
 root = Tk()
-root.geometry("260x390")
+root.title("DB Creator 1.0")
 
 myFrame = Frame(root)
 myFrame.grid(column=0, row=0)
@@ -16,7 +16,6 @@ variable_Name = StringVar()
 variable_Password = StringVar()
 variable_LastName = StringVar()
 variable_Address = StringVar()
-variable_Comments = StringVar()
 
 #--------------------Functions---------------------
 
@@ -26,18 +25,107 @@ def exitApp():
     if value == True:
         root.destroy()
 
-def insertData():
+def newDB():
     myConnection = sqlite3.connect("../../Programacion/Python/Usuarios")
+    myCursor = myConnection.cursor()
+
+    myCursor.execute('''CREATE TABLE USUARIOS(
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NOMBRE VARCHAR(50),
+        PASSWORD_ VARCHAR(50),
+        APELLIDOS VARCHAR(50),
+        ADDRESS_ VARCHAR(50),
+        COMMENT VARCHAR(100)
+    )''') 
+
+    myConnection.commit()
     myConnection.close()
+    messagebox.showinfo("Done", "New DataBase has been created successfully")
 
 def deleteTextFields():
+
     variable_Name.set("")
     variable_Password.set("")
     variable_LastName.set("")
     variable_Address.set("")
-    variable_Comments.set("")
-    textField_6.delete("1.0", END)
+    commentField.delete("1.0", END)
 
+def createDB():
+    myConnection = sqlite3.connect("../../Programacion/Python/Usuarios")
+    myCursor = myConnection.cursor()
+
+    values = (
+        textField_2.get(),
+        textField_3.get(),
+        textField_4.get(),
+        textField_5.get(),
+        commentField.get("1.0", END)
+    )
+
+    myCursor.execute("INSERT INTO USUARIOS VALUES (NULL,?,?,?,?,?)", values)
+    myConnection.commit()
+    myConnection.close()
+    messagebox.showinfo("Done","New register has been created successfully")
+    variable_Name.set("")
+    variable_Password.set("")
+    variable_LastName.set("")
+    variable_Address.set("")
+    commentField.delete("1.0", END) 
+
+def readDB():
+    try:
+        myConnection = sqlite3.connect("../../Programacion/Python/Usuarios")
+        myCursor = myConnection.cursor()
+        myCursor.execute("SELECT * FROM USUARIOS WHERE ID=?", textField_1.get())
+        values = myCursor.fetchall()
+
+        for user in values:
+            variable_Name.set(user[1])
+            variable_Password.set(user[2])
+            variable_LastName.set(user[3])
+            variable_Address.set(user[4])
+            commentField.delete("1.0", END)
+            commentField.insert(END,user[5])
+
+        myConnection.commit()
+        myConnection.close()
+    except sqlite3.ProgrammingError:
+        messagebox.showwarning("Warning","No \"ID\" element added")
+
+def updateDB():
+    if textField_1.get() != "":
+        myConnection = sqlite3.connect("../../Programacion/Python/Usuarios")
+        myCursor = myConnection.cursor()
+        values = (
+            textField_2.get(),
+            textField_3.get(),
+            textField_4.get(),
+            textField_5.get(),
+            commentField.get("1.0", END),
+            textField_1.get()
+        )
+        myCursor.execute("UPDATE USUARIOS SET NOMBRE=?, PASSWORD_=?, APELLIDOS=?, ADDRESS_=?, COMMENT=? WHERE ID=?", values)
+        myConnection.commit()
+        myConnection.close()
+        messagebox.showinfo("Done", "Register has been updated successfully")
+    else:
+        messagebox.showwarning("Warning","No \"ID\" element added")
+
+
+def deleteDB():
+    try:
+        value=messagebox.askyesno("Delete Resgister", "Are you sure?")
+        if value == True:
+            myConnection = sqlite3.connect("../../Programacion/Python/Usuarios")
+            myCursor = myConnection.cursor()
+            myCursor.execute("DELETE FROM USUARIOS WHERE ID=?", textField_1.get())
+            myConnection.commit()
+            myConnection.close()
+            messagebox.showinfo("Done", "Register deleted successfully")
+        else:
+            messagebox.showwarning("Cancelled", "Register has not been deleted")
+    except sqlite3.ProgrammingError:
+        messagebox.showwarning("Warning","No \"ID\" element added")
 
 #------------------------Menus------------------------
 
@@ -45,17 +133,17 @@ Menu_main = Menu(root)
 root.config(menu=Menu_main)
 Menu_DB = Menu(Menu_main, tearoff=0)
 Menu_main.add_cascade(label="DB", menu=Menu_DB)
-Menu_DB.add_command(label="Connect", command=insertData)
+Menu_DB.add_command(label="Connect", command=newDB)
 Menu_DB.add_command(label="Salir", command=exitApp)
 Menu_Borrar = Menu(Menu_main, tearoff=0)
 Menu_main.add_cascade(label="Borrar", menu=Menu_Borrar)
 Menu_Borrar.add_command(label="Borrar campos", command=deleteTextFields)
 Menu_CRUD = Menu(Menu_main, tearoff=0)
 Menu_main.add_cascade(label="CRUD", menu=Menu_CRUD)
-Menu_CRUD.add_command(label="Create")
-Menu_CRUD.add_command(label="Read")
-Menu_CRUD.add_command(label="Update")
-Menu_CRUD.add_command(label="Delete")
+Menu_CRUD.add_command(label="Create", command=createDB)
+Menu_CRUD.add_command(label="Read", command=readDB)
+Menu_CRUD.add_command(label="Update", command=updateDB)
+Menu_CRUD.add_command(label="Delete", command=deleteDB)
 Menu_Ayuda = Menu(Menu_main, tearoff=0)
 Menu_main.add_cascade(label="Ayuda", menu=Menu_Ayuda)
 Menu_Ayuda.add_command(label="Licencia")
@@ -75,8 +163,16 @@ textField_4 = Entry(myFrame, textvariable=variable_LastName)
 textField_4.grid(column=1, row=3, pady=10, padx=10)
 textField_5 = Entry(myFrame, textvariable=variable_Address)
 textField_5.grid(column=1, row=4, pady=10, padx=10)
-textField_6 = Text(myFrame,width=15, height=7)
-textField_6.grid(column=1, row=5, pady=10, padx=10)
+commentField = Text(myFrame, width=15, height=7)
+commentField.grid(column=1, row=5, pady=10, padx=10)
+
+
+#----------------------------Bar--------------------------
+
+
+bar=Scrollbar(myFrame, command=commentField.yview)
+bar.grid(row=5, column=2, sticky="nsew", pady=10)
+commentField.config(yscrollcommand=bar.set)
 
 
 #-------------------------Labels------------------------
@@ -92,10 +188,10 @@ Label(myFrame, text="Comentarios:").grid(column=0, row=5, sticky="e", pady=10, p
 #-------------------------Buttons------------------------
 
 
-Button(myFrame_2, text="Create").grid(column=0,row=0, pady=10, padx=10)
-Button(myFrame_2, text="Read").grid(column=1, row=0, pady=10, padx=10)
-Button(myFrame_2, text="Update").grid(column=2, row=0, pady=10, padx=10)
-Button(myFrame_2, text="Delete").grid(column=3, row=0, pady=10, padx=10)
+Button(myFrame_2, text="Create",command=createDB).grid(column=0,row=0, pady=10, padx=10)
+Button(myFrame_2, text="Read", command=readDB).grid(column=1, row=0, pady=10, padx=10)
+Button(myFrame_2, text="Update", command=updateDB).grid(column=2, row=0, pady=10, padx=10)
+Button(myFrame_2, text="Delete", command=deleteDB).grid(column=3, row=0, pady=10, padx=10)
 
 
 root.mainloop()
